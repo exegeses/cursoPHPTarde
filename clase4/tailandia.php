7af818afb261310b13b2c4aea8f583e442b54ad0
marcos pinardi <exegeses@gmail.com>
2020-05-06 15:21:10 -0300

    material clase 4

---
+++ clase4/tailandia.php
@@ -0,0 +1,47 @@
+<?php
+
+    $locacion = ["chinatown",
+        "floating-market",
+        "hat-laem",
+        "wat-arun",
+        "khao-san-road",
+        "ko-khai-beach",
+        "kwai-river",
+        "limestone",
+        "longtail-boat",
+        "maya-bay",
+        "pra-nang",
+        "rai-leh-beach",
+        "rawai",
+        "sirocco",
+        "tuk-tuk",
+        "wat-saket"
+    ];
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <main class="container">
+        <h1>Locaciones en Tailandia</h1>
+
+
+        <article class="card col-3 p-0">
+            <img src="tailandia/muestra.png" class="card-img-top">
+            <div class="card-body">
+                nombre de la locación
+            </div>
+        </article>
+
+    </main>
+
+</body>
+</html>
