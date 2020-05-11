71679f665ba908aec399f56015f0951d964c02b5
marcos pinardi <exegeses@gmail.com>
2020-05-11 18:00:02 -0300

    proyecto integrador con info de Trello

---
+++ sistema/README.md
@@ -0,0 +1,6 @@
+# Proyecto integrador
+
+    Trello del proyecto  
+    https://trello.com/invite/b/OIhDP1wx/109625d70b5cec926b073bf48b7b5da5/catalogophptarde
+    
+    
--- sistema/includes/nav.php
+++ sistema/includes/nav.php
@@ -8,29 +8,21 @@
         <a class="py-2" href="adminCategorias.php">Categorías</a>
         <a class="py-2" href="adminProductos.php">Productos</a>
         <a class="py-2" href="adminUsuarios.php">Usuarios</a>
-        categorias
-<?php
-        if ( !isset( $_SESSION['login'] ) ){  
-?>
-        
+
+
         <button class="btn btn-dark">
             <a href="formLogin.php"><i class="fas fa-sign-in-alt mr-2"></i> Ingresar</a>
         </button>
-<?php
-        }
-        else{
-?>        
-        
+
+<!--
                         <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <a href="#"><i class="fas fa-sign-out-alt"></i> <?= $_SESSION['nombreApellido']; ?> </a>
+                            <a href="#"><i class="fas fa-sign-out-alt"></i> Nombre y Apellido </a>
                         </button>
                         <div class="dropdown-menu bg-dark" aria-labelledby="dropdownMenuButton">
                             <a class="dropdown-item bg-dark" href="logout.php">Salir</a>
                             <a class="dropdown-item bg-dark" href="formContacto.php">Contacto</a>
                             <a class="dropdown-item bg-dark" href="#">Acción 2</a>
                         </div>
-<?php
-        }
-?>
+-->
     </div>
 </nav>
