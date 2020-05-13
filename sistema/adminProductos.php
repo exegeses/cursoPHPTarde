d733f2ac6e55890933f9707660b5906bf3eabe92
marcos pinardi <exegeses@gmail.com>
2020-05-13 16:42:10 -0300

    vista de panel de productos

---
+++ sistema/adminProductos.php
@@ -0,0 +1,57 @@
+<?php
+    #require 'config/config.php';
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Panel de administración de productos</h1>
+
+        <a href="admin.php" class="btn btn-outline-secondary mb-3">
+            volver a dashboard
+        </a>
+
+        <table class="table table-striped table-bordered table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Producto</th>
+                    <th>Precio</th>
+                    <th>Marca</th>
+                    <th>Categoria</th>
+                    <th>Presentación</th>
+                    <th>Imagen</th>
+                    <th colspan="2">
+                        <a href="" class="btn btn-dark">
+                            Agregar
+                        </a>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>nombre</td>
+                    <td>precio</td>
+                    <td>marca</td>
+                    <td>categoria</td>
+                    <td>pres</td>
+                    <td>imagen</td>
+                    <td>
+                        <a href="" class="btn btn-outline-secondary">
+                            Modificar
+                        </a>
+                    </td>
+                    <td>
+                        <a href="" class="btn btn-outline-secondary">
+                            Eliminar
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <a href="admin.php" class="btn btn-outline-secondary">
+            volver a dashboard
+        </a>
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
