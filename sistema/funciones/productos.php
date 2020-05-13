8786ae1d53f337086eaa920fa3038b9ddbdad507
marcos pinardi <exegeses@gmail.com>
2020-05-13 17:11:32 -0300

    CRUD productos

--- sistema/funciones/productos.php
+++ sistema/funciones/productos.php
@@ -3,6 +3,20 @@
     ################################
     ###### CRUD de productos ######
 
+    function listarProductos()
+    {
+        $link = conectar();
+        $sql= "SELECT 
+                        idProducto, prdNombre, prdPrecio, 
+                        idMarca, idCategoria, 
+                        prdPresentacion, prdStock, 
+                        prdImagen 
+                 FROM 
+                        productos";
+        $resultado = mysqli_query( $link, $sql );
+        return $resultado;
+    }
+
     /**
      * listarProductos()
      * verProductoPorID()
