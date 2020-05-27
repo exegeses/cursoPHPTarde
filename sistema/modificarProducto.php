ac29b95dbf0b1a3a105350588b1ea797e9b0ede7
marcos pinardi <exegeses@gmail.com>
2020-05-27 15:43:25 -0300

    listo app para modificar producto

--- sistema/modificarProducto.php
+++ sistema/modificarProducto.php
@@ -1,11 +1,29 @@
 <?php
     #require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+        $chequeo = modificarProducto();
 	include 'includes/header.html';  
 	include 'includes/nav.php';  
 ?>
 
     <main class="container">
         <h1>Modificación de un producto</h1>
+<?php
+        $colorDiv = 'danger';
+        $mensaje = 'No se pudo modificar el producto,';
+
+        if( $chequeo ){
+            $colorDiv = 'success';
+            $mensaje = 'Producto modificado correctamente.';
+        }
+?>
+        <div class="alert alert-<?= $colorDiv ?>">
+            <?= $mensaje ?>
+            <a href="adminProductos.php" class="btn btn-light">
+                volver a panel
+            </a>
+        </div>
 
     </main>
 
